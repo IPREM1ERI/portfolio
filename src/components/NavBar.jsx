25f4891e2d034aa1ed0d8c8182265578bd66ad6d
davidv <davidvazquezgit@gmail.com>
2022-11-16 11:57:28 -0800

    work on nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
+import styles from "./NavBar.module.css"
 
 function NavBar() {
   return (
-    <nav>
-        <Link to='/'>Home</Link>
-        <ul>
-            <Link to='/about'>About</Link>
-            <Link to='/projects'>Projects</Link>
-            <Link to='/contact'>Contact</Link>
-            <Link to='/resume'>Resume</Link>
+    <nav >
+        <ul className={styles.navBar}>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/projects'>Projects</Link>
+          <Link to='/contact'>Contact</Link>
+          <Link to='/resume'>Resume</Link>
         </ul>
     </nav>
   )
