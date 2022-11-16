9bf535eee2defa55531ca7d2d8422edb1840f288
davidv <davidvazquezgit@gmail.com>
2022-11-16 12:24:44 -0800

    push Code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,16 @@ import styles from "./NavBar.module.css"
 
 function NavBar() {
   return (
-    <nav >
-        <ul className={styles.navBar}>
+    <header>
+
+    <nav className={styles.navBar}>
           <Link to='/'>Home</Link>
           <Link to='/about'>About</Link>
           <Link to='/projects'>Projects</Link>
           <Link to='/contact'>Contact</Link>
           <Link to='/resume'>Resume</Link>
-        </ul>
     </nav>
+    </header>
   )
 }
 
