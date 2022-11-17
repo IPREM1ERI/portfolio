a2eaf059ec2efb76f0b41c3c534fb630d5ac27e3
davidv <davidvazquezgit@gmail.com>
2022-11-17 10:46:59 -0800

    remove underline from a tags in nav bar, change color

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,14 +4,13 @@ import styles from "./NavBar.module.css"
 function NavBar() {
   return (
     <header>
-
-    <nav className={styles.navBar}>
-          <Link to='/'>Home</Link>
-          <Link to='/about'>About</Link>
-          <Link to='/projects'>Projects</Link>
-          <Link to='/contact'>Contact</Link>
-          <Link to='/resume'>Resume</Link>
-    </nav>
+      <nav className={styles.navBar}>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/projects'>Projects</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='/resume'>Resume</Link>
+      </nav>
     </header>
   )
 }
