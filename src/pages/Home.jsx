93615272eb90d16a9dfbe88924050331ef51926a
davidv <davidvazquezgit@gmail.com>
2022-11-17 16:21:18 -0800

    finish homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,22 @@ import { Link } from "react-router-dom"
 
 function Home() {
   return (
-    <> 
-      <div className={styles.homeContainer}>
+      <container className={styles.homeContainer}>
+        <section>
         <Link to='/about'>
-          <h1>DAVID VAZQUEZ</h1><br/>
-          <img src="http://www.avatarsinpixels.com/chibi/eyJTaG9lcyI6IjQiLCJQYW50cyI6IjE3IiwiVG9wIjoiMTAiLCJNb3V0aCI6IjYiLCJIYWlyVG9wIjoiNiIsInNraW5Ub25lIjoiYzI4ODM3IiwiaGFpclRvbmUiOiIzMzMzMzMiLCJleWVzVG9uZSI6ImU2Y2NhOCIsInBhbnRzVG9uZTIiOiJjY2NjY2MiLCJ0b3BUb25lMiI6ImNhZmFkNCJ9/1/show.png" alt="profile-img" />
-          <h3>A</h3>
+          <h1>DAVID VAZQUEZ</h1>
+          <img src="/homepageImg.PNG" alt="profile-img" />
           <h2>SOFTWARE ENGINEER</h2>
-          <h4>Int:4 Str:2 Luck:1 Dex:3 Char:5</h4>
         </Link>
-      </div> 
-    </>
+          <div>
+            <h4 id="int">Int:3</h4>
+            <h4 id="str">Str:2</h4>
+            <h4 id="dex">Dex:3</h4>
+            <h4 id="char">Char:5</h4>
+            <h4 id="luck">Luck:1</h4>
+          </div>
+        </section>
+      </container> 
   )
 }
 
