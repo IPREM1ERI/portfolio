aa47c7db94115447326b18d1b04659dba8ef0958
davidv <davidvazquezgit@gmail.com>
2022-11-18 11:18:38 -0800

    split navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,19 +3,18 @@ import styles from "./NavBar.module.css"
 
 function NavBar() {
   return (
-    <header>
-      <nav className={styles.navBar}>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/projects'>Projects</Link>
-        <Link to='/contact'>Contact</Link>
-        <Link to='/resume'>Resume</Link>
-        <div className="git_linkedin">
-          <a href="https://www.linkedin.com/in/davidvazquez88/">linkedin</a>
-          <a href="https://github.com/IPREM1ERI">GitHub</a>
-        </div>
-      </nav>
-    </header>
+    <nav className={styles.navBar}>
+      <h3>DAVID VAZQUEZ</h3>
+      <div id="git_linkedin">
+        <a href="https://www.linkedin.com/in/davidvazquez88/">linkedin</a>
+        <a href="https://github.com/IPREM1ERI">GitHub</a>
+      </div>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/projects'>Projects</Link>
+      <Link to='/contact'>Contact</Link>
+      <Link to='/resume'>Resume</Link>
+    </nav>
   )
 }
 
