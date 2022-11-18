edae5cdfc54f2fb438251d2a7df191d2ab1cedc2
davidv <davidvazquezgit@gmail.com>
2022-11-18 12:33:39 -0800

    finish navbar/homepage work on contact page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,25 @@
 import { Link } from "react-router-dom";
 import styles from "./NavBar.module.css"
+// import { FaGithub, FaLinkedinIn, FaAt } from 'react-icons/fa'
 
 function NavBar() {
   return (
     <nav className={styles.navBar}>
-      <h3>DAVID VAZQUEZ</h3>
+      <h2>DAVID VAZQUEZ</h2>
       <div id="git_linkedin">
-        <a href="https://www.linkedin.com/in/davidvazquez88/">linkedin</a>
-        <a href="https://github.com/IPREM1ERI">GitHub</a>
+        <a className="fa-brands fa-linkedin" href="https://www.linkedin.com/in/davidvazquez88/"></a>
+        <a className="fa-brands fa-github" href="https://github.com/IPREM1ERI"  ></a>
       </div>
       <Link to='/'>Home</Link>
       <Link to='/about'>About</Link>
       <Link to='/projects'>Projects</Link>
-      <Link to='/contact'>Contact</Link>
       <Link to='/resume'>Resume</Link>
+      <Link to='/contact'>Contact</Link>
     </nav>
   )
 }
 
 export default NavBar
+
+
+
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom"
+import  styles from  "./Contact.module.css"
 
 function Contact() {
   return (
-    <>
+    <section className={styles.contact}>
       <h1>Contact</h1>
-      <p>"Reach out if you'd like ro work with me, mayne"</p>
-      {/* <a href="https://www.linkedin.com/in/davidvazquez88/">linkedin</a>
-      <a href="https://github.com/IPREM1ERI">GitHub</a> */}
-      <a href="mailto:d.vazquez0822@gmail.com">Email David</a>
-    </>  
+      <ul>
+        <li>"I am currently searching for my next opportunity"</li>
+        <li>If you either have the perfect job or just want to say hi,</li>
+        <li>here's how to connect:</li>
+      </ul>
+      <div>
+        <a className="fa-brands fa-linkedin" href="https://www.linkedin.com/in/davidvazquez88/"> LinkedIn</a>
+          <a className="fa-brands fa-github" href="https://github.com/IPREM1ERI"  > GitHub</a>
+        <a className="fa-solid fa-at" href="mailto:d.vazquez0822@gmail.com"> Email</a>
+      </div>
+    </section>  
   )
 }
 
 export default Contact
+
