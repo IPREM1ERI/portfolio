8d43dfbfdfbaaa8cf60712927e96ebe80a1d0c7c
davidv <davidvazquezgit@gmail.com>
2022-11-17 17:15:29 -0800

    update homepage css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,20 @@ import { Link } from "react-router-dom"
 
 function Home() {
   return (
-      <container className={styles.homeContainer}>
+      <div className={styles.homeContainer}>
+        <section>
+          <h1 id="locked">LOCKED</h1>
+          <h1>ALT CUNNINGHAM</h1>
+          <img src="/AltC_img.PNG" alt="profile-img" />
+          <h2>NETRUNNER</h2>
+          <div>
+            <h4 id="int">Int:5</h4>
+            <h4 id="str">Str:2</h4>
+            <h4 id="dex">Dex:5</h4>
+            <h4 id="char">Char:5</h4>
+            <h4 id="luck">Luck:4</h4>
+          </div>
+        </section>
         <section>
         <Link to='/about'>
           <h1>DAVID VAZQUEZ</h1>
@@ -18,7 +31,22 @@ function Home() {
             <h4 id="luck">Luck:1</h4>
           </div>
         </section>
-      </container> 
+        <section>
+          <h1 id="locked">LOCKED</h1>
+          <h1>RACHE BARTMOSS</h1>
+          <img src="/rache_img.PNG" alt="profile-img" />
+          <h2>NETRUNNER</h2>
+          <div>
+            <h4 id="int">Int:5</h4>
+            <h4 id="str">Str:3</h4>
+            <h4 id="dex">Dex:4</h4>
+            <h4 id="char">Char:4</h4>
+            <h4 id="luck">Luck:5</h4>
+          </div>
+        </section>
+
+        
+      </div> 
   )
 }
 
