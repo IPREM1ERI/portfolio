234ef7986ff1c147b2d58f12ca866de6d1d8fda8
davidv <davidvazquezgit@gmail.com>
2022-11-19 13:03:39 -0800

    finish about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,15 +3,13 @@ import  styles from  "./About.module.css"
 function About() {
   return (
     <>
-    <main className={styles.about}>
-      <section>
-        <h1>Picture 1</h1>
+      <main className={styles.about}>
+        <div>
+          <img src="./IMG_1552.jpg" alt="" />
+        </div>
+        <section>
           <p>My journey in the worlds of the military and hospitality industries has helped me cultivate a coding philosophy that emphasizes writing clean, functional code that provides a fun and user-friendly experience.My experience in those fields as well as my  love for all things tech, drives me to keep learning for ways to keep improving my coding philosophy.</p>
-      </section>
-      <section>
-        <h1>Picture 2</h1>
-        <p>I spend my free time delving into my interest. Playing games like Lost Ark, Halo Infinte, Warzone, roulette in silver/gold lobbies in Valorant praying for competent teammates, building a new keyboard while listening to music, or catch up on reading comics/manga while a Dodger or Laker game plays in the background.</p>
-      </section>
+          <p>In my free time I delve into my interest. Playing games like Lost Ark, Halo Infinte, Warzone, roulette in silver/gold lobbies in Valorant praying for competent teammates, building a new keyboard while listening to music, or catch up on reading comics/manga while a Dodger or Laker game plays in the background. And now I can add messing around wiht code to my interest.</p>
         <h1>Software Development Skills</h1>
           <ul>
             <li className="fa-brands fa-square-js"></li>
@@ -21,9 +19,9 @@ function About() {
             <li className="fa-brands fa-react"></li>
             <li className="fa-brands fa-node-js"></li>
           </ul>
-        
-    </main>
-      </>
+        </section>
+      </main>
+    </>
   )
 }
 
