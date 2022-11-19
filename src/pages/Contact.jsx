f45ad93b4ccfcc5957254be77e3a254e7fab83db
davidv <davidvazquezgit@gmail.com>
2022-11-18 17:27:12 -0800

    finish resume page, adjust contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,19 +3,28 @@ import  styles from  "./Contact.module.css"
 
 function Contact() {
   return (
-    <section className={styles.contact}>
+    <main className={styles.contact}>
       <h1>GET IN TOUCH</h1>
       <ul>
         <li>"I am currently searching for my next opportunity"</li>
         <li>If you either have the perfect job or just want to say hi,</li>
         <li>here's how to connect:</li>
       </ul>
-      <div>
-        <a className="fa-brands fa-linkedin" href="https://www.linkedin.com/in/davidvazquez88/"> LinkedIn</a>
-          <a className="fa-brands fa-github" href="https://github.com/IPREM1ERI"  > GitHub</a>
-        <a className="fa-solid fa-at" href="mailto:d.vazquez0822@gmail.com"> Email</a>
-      </div>
-    </section>  
+      <section>
+        <div>
+          <a className="fa-brands fa-linkedin" href="https://www.linkedin.com/in/davidvazquez88/"></a>
+          <a className="linkNames"href="https://www.linkedin.com/in/davidvazquez88/">LinkedIn</a>
+        </div>
+        <div>
+          <a className="fa-brands fa-github" href="https://github.com/IPREM1ERI"  ></a>
+          <a className="linkNames" href="https://github.com/IPREM1ERI">GitHub</a>
+        </div>
+        <div>
+          <a className="fa-solid fa-at" href="mailto:d.vazquez0822@gmail.com"></a>
+          <a className="linkNames" href="mailto:d.vazquez0822@gmail.com">Email</a>
+        </div>
+      </section>
+    </main>  
   )
 }
 
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,21 @@
+import styles from "./Resume.module.css"
 function Resume() {
   return (
-      <h1>Resume</h1>
+    <main className={styles.resume}>
+      <embed 
+        type="application/pdf"
+        src="./David-Vazquez-Resume.pdf"
+        // frameBorder="0"
+        scrolling="auto"
+        height="500%"
+        width="100%"
+      />
+      <div className="buttonDiv">
+        <button>
+        <a href={require("./Resume/David-Vazquez-Resume.pdf")} download="David Vazquez's Resume">Download Resume</a>
+        </button>
+      </div>
+    </main>
   )
 }
 
